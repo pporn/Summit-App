d2b9b8882b09446765d5f28c20ad34f59a604db3
Chen Yang <34663197+pporn@users.noreply.github.com>
2018-11-11 13:22:15 -0600

    Refactor all functions (#4)
    * refactor all functions
    
    * add cors policy for error codes

--- functions/index.js
+++ functions/index.js
@@ -23,7 +23,9 @@ exports.addUser = functions.https.onRequest((req, res) => {
     'date_of_birth': date_of_birth,
   });
 
-  return res.status(200).send('OK');
+  return cors(req, res, () => {
+    res.status(200).send('OK');
+  });
 });
 
 /**************************/
@@ -31,35 +33,45 @@ exports.addUser = functions.https.onRequest((req, res) => {
 /**************************/
 
 exports.authUser = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    const first_name = req.query.first_name;
-    const last_name = req.query.last_name;
-    const date_of_birth = req.query.dob;
+  const first_name = req.query.first_name;
+  const last_name = req.query.last_name;
+  const date_of_birth = req.query.dob;
 
-    const query = admin.database().ref('user');
-    let userId = '';
+  const query = admin.database().ref('user');
 
-    return query.once('value')
-      .then((snapshot) => {
-        // iterate through all the entries
-        snapshot.forEach(entry => {
-          if(entry.child('date_of_birth').val() === date_of_birth
-            &&
-            entry.child('first_name').val() === first_name
-            &&
-            entry.child('last_name').val() === last_name
-          ) {
-            return res.status(200).send({'id': entry.key});
-          }
-        });
+  query.once('value')
+    .then((snapshot) => {
+      let userId;
 
-        // return 422 if cannot find given user
-        return res.status(422).send({'message': 'Fail to auth given user'});
-      }).catch(error => {
-        // return 422 for any other reason
-        return res.status(422).send({'message': 'Fail to auth given user'});
+      // iterate through all the entries
+      snapshot.forEach(entry => {
+        if(entry.child('date_of_birth').val() === date_of_birth
+          &&
+          entry.child('first_name').val() === first_name
+          &&
+          entry.child('last_name').val() === last_name
+        ) {
+          userId = entry.key;
+        }
       });
-  });
+
+      // return user id if given user is found
+      if(userId) {
+        return cors(req, res, () => {
+          res.status(200).send({'id': userId});
+        });
+      }
+
+      // return 422 if cannot find given user
+      return cors(req, res, () => {
+        res.status(422).send({'message': 'Fail to auth given user'});
+      });
+    }).catch(error => {
+      // return 422 for any other reason
+      return cors(req, res, () => {
+        res.status(422).send({'message': 'Fail to auth given user'});
+      });
+    });
 });
 
 /**************************/
@@ -67,35 +79,46 @@ exports.authUser = functions.https.onRequest((req, res) => {
 /**************************/
 
 exports.getUserId = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    const first_name = req.query.first_name;
-    const last_name = req.query.last_name;
-    const date_of_birth = req.query.dob;
+  const first_name = req.query.first_name;
+  const last_name = req.query.last_name;
+  const date_of_birth = req.query.dob;
 
-    const query = admin.database().ref('user');
-    let userId = '';
+  const query = admin.database().ref('user');
 
-    return query.once('value')
-      .then(snapshot => {
-        // iterate through all the entries
-        snapshot.forEach((entry) => {
-          if(entry.child('date_of_birth').val() === date_of_birth
-            &&
-            entry.child('first_name').val() === first_name
-            &&
-            entry.child('last_name').val() === last_name
-          ) {
-            return res.status(200).send({'id': entry.key});
-          }
-        });
+  query.once('value')
+    .then(snapshot => {
+      let userId;
 
-        // return 422 if cannot find given user
-        return res.status(422).send({'message': 'Fail to get user id'});
-      }).catch(error => {
-        // return 422 for any other reason
-        return res.status(422).send({'message': 'Fail to get user id'});
+      // iterate through all the entries
+      snapshot.forEach((entry) => {
+        // check all entries
+        if(entry.child('date_of_birth').val() === date_of_birth
+          &&
+          entry.child('first_name').val() === first_name
+          &&
+          entry.child('last_name').val() === last_name
+        ) {
+          userId = entry.key;
+        }
       });
-  });
+
+      // return user id if user is valid
+      if(userId) {
+        return cors(req, res, () => {
+          res.status(200).send({'id': userId});
+        });
+      }
+
+      // return 422 if cannot find given user
+      return cors(req, res, () => {
+        res.status(422).send({'message': 'Fail to get user id'});
+      });
+    }).catch(error => {
+      // return 422 for any other reasons
+      return cors(req, res, () => {
+        res.status(422).send({'message': 'Fail to get user id'});
+      });
+    });
 });
 
 /**************************/
@@ -103,42 +126,52 @@ exports.getUserId = functions.https.onRequest((req, res) => {
 /**************************/
 
 exports.getUserInfo = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    const userId = req.query.user_id;
+  const userId = req.query.user_id;
 
-    const getUserInfo = admin.database().ref('user').child(userId);
-    getUserInfo.once('value')
-      .then(snapshot => {
-        // check whether user id exits
-        if(!snapshot.exists()) {
-          return res.status(422).send({'message': 'User id not found'});
-        }
-        return res.status(200).send(snapshot.toJSON());
-      }).catch(error => {
+  const getUserInfo = admin.database().ref('user').child(userId);
+  getUserInfo.once('value')
+    .then(snapshot => {
+      // check whether user id exits
+      if(!snapshot.exists()) {
+        return cors(req, res, () => {
+          res.status(422).send({'message': 'User id not found'});
+        });
+      }
+
+      // return all user informations
+      return cors(req, res, () => {
+        res.status(200).send(snapshot.toJSON());
+      });
+    }).catch(error => {
+      return cors(req, res, () => {
         res.status(422).send({'message': 'Fail to get user info'});
       });
-  });
+    });
 });
 
 // fetch all user names from database
 exports.getAllUserNames = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    const allUsers = admin.database().ref('user');
-    allUsers.once('value')
-      .then(snapshot => {
-        const names = [];
+  const allUsers = admin.database().ref('user');
 
-        snapshot.forEach((entry) => {
-          //get user info
-          const userInfo = entry.val();
-          //push fullname
-          names.push(userInfo.first_name + ' ' + userInfo.last_name);
-        });
-        // return all user names
-        return res.status(200).send(JSON.stringify(names));
-      }).catch(error => {
+  allUsers.once('value')
+    .then(snapshot => {
+      const names = [];
+      
+      snapshot.forEach((entry) => {
+        //get user info
+        const userInfo = entry.val();
+        //push fullname
+        names.push(userInfo.first_name + ' ' + userInfo.last_name);
+      });
+
+      // return all user names
+      return cors(req, res, () => {
+        res.status(200).send(JSON.stringify(names));
+      });
+    }).catch(error => {
+      return cors(req, res, () => {
         res.status(422).send({'message': 'Fail to get user info'});
       });
-  });
+    });
 });
 
