b59b4f3cc40edc46cfaae12e986cf50ed8fb2cbb
Gogs <gogs@fake.local>
2018-12-06 12:19:52 -0600

    add Punch Card Balance

--- app/src/components/ClientAccount/ClientAccountContainer.jsx
+++ app/src/components/ClientAccount/ClientAccountContainer.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import getUserInfo from './ClientAccountVirtualController'
 import getLastCheckIn from './ClientAccountCheckInVirtualController.jsx'
+import getPunchCard from './ClientAccountPunchVirtualController.jsx'
 class ClientAccountContainer extends Component {
     constructor(props) {
         super(props)
@@ -9,10 +10,12 @@ class ClientAccountContainer extends Component {
             userId: 'N/A',
             userInfo: undefined,
             last_check_in: undefined,
+            punch: undefined,
         }
 
         this.onFinishUserInfo = this.onFinishUserInfo.bind(this);
         this.onFinishLastCI = this.onFinishLastCI.bind(this);
+        this.onFinishPunch = this.onFinishPunch.bind(this);
     }
 
     componentDidMount() {
@@ -24,6 +27,7 @@ class ClientAccountContainer extends Component {
 
         getUserInfo(userId, this.onFinishUserInfo);
         getLastCheckIn(userId, this.onFinishLastCI);
+        getPunchCard(userId, this.onFinishPunch);
     }
 
     onFinishUserInfo(userInfo) {
@@ -33,24 +37,28 @@ class ClientAccountContainer extends Component {
     }
 
     onFinishLastCI(checkInInfo) {
-        console.log(checkInInfo);
         this.setState({
             last_check_in : checkInInfo,
-        })
+        });
     }
-
+    onFinishPunch(punchTime){
+       this.setState({
+        punch : punchTime,
+       });
+    }
     render() {
         if(this.state.userInfo) {
             const { first_name, last_name, date_of_birth} = this.state.userInfo;
             const last_check_in = this.state.last_check_in;
             const userName = first_name + ' ' + last_name;
-
+            const punch = this.state.punch;
             return(
                 <div>
                     <h2>Client Account Page</h2>
                     <h3>Name: {userName}</h3>
                     <h3>Date of Birth: {date_of_birth}</h3>
                     <h3>Last Check-in: {last_check_in}</h3>
+                    <h3>Punch Card Balance: {punch} </h3>
                 </div>
             );
         }
@@ -64,3 +72,4 @@ class ClientAccountContainer extends Component {
 }
 
 export default ClientAccountContainer;
+
---
+++ app/src/components/ClientAccount/ClientAccountPunchVirtualController.jsx
@@ -0,0 +1,32 @@
+
+import { queryBuilder } from '../Shared/Utils.js';
+function getPunchCard(userId, callback) {
+    // construct query
+    const api = 'https://us-central1-summit-app-6f288.cloudfunctions.net/getPunchCard?';
+    const query = queryBuilder(api)
+                    .param('user_id').val(userId)
+                    .getUrl();
+
+    // query
+    try {
+        fetch(query).then(res => {
+            // return true upon successful call
+            if(res.status === 200) {
+                res = res.json();
+
+                res.then((data) => {
+                    callback(data);
+                });
+            } else {
+                return null;
+            }
+        }).catch(error => {
+            return null;
+        });
+    } catch (e) {
+        return null;
+    }
+}
+
+export default getPunchCard;
+
