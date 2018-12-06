34f3823390b49850f31eb8a2c5433852bcda338a
Jason <jx240230@gmail.com>
2018-12-06 11:56:50 -0600

    Revert "Add getPunchCard"

--- app/src/components/ClientAccount/ClientAccountVirtualController.jsx
+++ app/src/components/ClientAccount/ClientAccountVirtualController.jsx
@@ -1,4 +1,5 @@
 import { queryBuilder } from '../Shared/Utils.js';
+
 function getUserInfo(userId, callback) {
     // construct query
     const api = 'https://us-central1-summit-app-6f288.cloudfunctions.net/getUserInfo?';
