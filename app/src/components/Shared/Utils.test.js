1998e15d173b9e531944e05239a7c9bd474f2c7c
Chen Yang <chen@pporn.tv>
2018-12-11 18:23:54 -0600

    make sure Utils.js test coverage is 100

--- app/src/components/Shared/Utils.test.js
+++ app/src/components/Shared/Utils.test.js
@@ -1,17 +1,27 @@
 const { verifyName, printDate, queryBuilder } = require("./Utils.js")
 
 describe("verifyName", () => {
-  it("When person's first name and last name is non-sense", () => {
+  it("When person's name is non-sense", () => {
     expect(verifyName("1232141 123123")).toBe("");
   });
 
-  it("When person's first name and last name is good", () => {
+  it("When person's name is null", () => {
+    expect(verifyName(null)).toBe("");
+  });
+
+  it("When person's name is good", () => {
     expect(verifyName("John Doe")).toBe("John Doe");
   });
 });
 
-test("printDate", () => {
-  expect(printDate(new Date(2010,1,1))).toBe("2/1/2010")
+describe("printDate", () => {
+  it("When it is a normal date", () => {
+    expect(printDate(new Date(2010,1,1))).toBe("2/1/2010");
+  });
+
+  it("When date is null", () => {
+    expect(printDate(null)).toBe("");
+  });
 });
 
 test("queryBuilder", () => {
