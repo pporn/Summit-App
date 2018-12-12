ce6231ff03b0947fd2b3f88ba571d194e64fee2e
Chen Yang <chen@pporn.tv>
2018-12-11 18:18:17 -0600

    add code coverage

--- app/src/components/Shared/Utils.test.js
+++ app/src/components/Shared/Utils.test.js
@@ -1,11 +1,21 @@
 const { verifyName, printDate, queryBuilder } = require("./Utils.js")
 
-test("test", () => {
-  expect(verifyName("")).toBe("")
-})
+describe("verifyName", () => {
+  it("When person's first name and last name is non-sense", () => {
+    expect(verifyName("1232141 123123")).toBe("");
+  });
 
-test("test2", () => {
+  it("When person's first name and last name is good", () => {
+    expect(verifyName("John Doe")).toBe("John Doe");
+  });
+});
+
+test("printDate", () => {
   expect(printDate(new Date(2010,1,1))).toBe("2/1/2010")
-})
+});
 
+test("queryBuilder", () => {
+  expect(queryBuilder('test.com?').param('a').val('b').getUrl()).toBe("test.com?a=b");
+  expect(queryBuilder('test.com?').param('a').val('b').param('c').val('d').getUrl()).toBe("test.com?a=b&c=d");
+});
 
