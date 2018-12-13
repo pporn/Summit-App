441a7ce6653a5b26a003ff48a639a2f4b4bf404c
Chen Yang <chen@pporn.tv>
2018-12-13 11:40:40 -0600

    set up onsen ui

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,16 @@
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
 import React from 'react';
+import { Page, Button } from 'react-onsenui';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <Page>
+        <Button> Tap me!</Button>
+      </Page>
+    </div>
   );
 }
 
